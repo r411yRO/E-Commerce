3953da1a16807e36e732d543080af15b43b41e8c
r411yRO <danieldraghicilst@gmail.com>
2023-07-01 17:14:08 +0300

    Am creat baza de date si template-urile pentru pagina principala v2

---
+++ src/main/java/com/example/demo/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+	User findByEmail(String email);
+
+    @Override
+    void delete(User user);
+
+}
