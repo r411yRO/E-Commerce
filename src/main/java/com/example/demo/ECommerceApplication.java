d49373283bdca82a5932ced063986923755dafb6
Daniel <danieldraghicilst@gmail.com>
2023-07-08 11:59:42 +0300

    Fixed database

--- src/main/java/com/example/demo/ECommerceApplication.java
+++ src/main/java/com/example/demo/ECommerceApplication.java
@@ -6,11 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-<<<<<<< HEAD
-//@ComponentScan(basePackages = "com.example")
-=======
 @ComponentScan(basePackages = "com.example")
->>>>>>> 3d279c548b61ac11bbce81d241f5c18306be921b
 public class ECommerceApplication {
 
 	public static void main(String[] args) throws IOException {
